e37b537fcac454013656f3cf27aa1d6af8a5bfe6
jakaria6284 <kamal.jakaria.6284mia@gmail.com>
2023-10-20 14:17:30 +0600

    second commit

---
+++ app/src/main/java/com/example/a75dayshardchallenge/homeeActivity.java
@@ -0,0 +1,43 @@
+package com.example.a75dayshardchallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class homeeActivity extends AppCompatActivity {
+TextView textView;
+Calendar calendar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_homee);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(getResources().getColor(R.color.black));
+        }
+        calendar=Calendar.getInstance();
+        textView=findViewById(R.id.currentdate);
+        Date currentDate = calendar.getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("EEEE d MMMM yyyy", Locale.getDefault());
+        String formattedDate = sdf.format(currentDate);
+        textView.setText(formattedDate);
+
+    }
+}
